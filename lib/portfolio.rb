b321658540d7e4077928b0997de2e2c1fb5fc7cf
danielfromCL <81190002+danielfromCL@users.noreply.github.com>
2024-08-16 02:01:26 -0400

    feat(Portfolio): add annualized return to profit method, plus tests for it's implementation

--- lib/portfolio.rb
+++ lib/portfolio.rb
@@ -11,13 +11,26 @@ class Portfolio
   # Calculates the portfolio's profit for a given date range
   # @param from_date [Date] the start date
   # @param to_date [Date] the end date
-  # @return [BigDecimal] the profit
+  # @return [Hash] a hash containing the net profit for the given range, and the annualized return between said dates
   def profit(from_date, to_date)
     profit = BigDecimal(0)
+    initial_value = BigDecimal(0)
+    end_value = BigDecimal(0)
     @stocks.each do |stock|
       # the profit of each stock between two dates is the difference between the price at the end date and the price at the start date
       profit += stock.price(to_date) - stock.price(from_date)
+      initial_value += stock.price(from_date)
+      end_value += stock.price(to_date)
     end
-    profit
+    # Convert difference between dates into year format
+    year_percentage = (to_date - from_date) / BigDecimal(365)
+    # Only calculate return if starting price was not 0 or some time has passed, since division by 0 is not allowed
+    annualized_return = if year_percentage.zero? || initial_value.zero?
+                          0
+                        else
+                          (end_value / initial_value - 1) / year_percentage
+                        end
+
+    { net_profit: profit, annualized_return: }
   end
 end
--- spec/portfolio_spec.rb
+++ spec/portfolio_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Portfolio do
     stock_2 = Stock.new(2)
     stock_3 = Stock.new(3)
     date_1 = Date.new(2021, 1, 1)
-    date_2 = Date.new(2021, 1, 2)
+    date_2 = Date.new(2021, 7, 16)
     s1_price_1 = stock_1.price(date_1)
     s2_price_1 = stock_2.price(date_1)
     s3_price_1 = stock_3.price(date_1)
@@ -27,14 +27,39 @@ RSpec.describe Portfolio do
     s2_price_2 = stock_2.price(date_2)
     s3_price_2 = stock_3.price(date_2)
     portfolio = Portfolio.new([stock_1, stock_2, stock_3])
+    net_profit = (s1_price_2 - s1_price_1) + (s2_price_2 - s2_price_1) + (s3_price_2 - s3_price_1)
+    initial_value = s1_price_1 + s2_price_1 + s3_price_1
+    end_value = s1_price_2 + s2_price_2 + s3_price_2
+    annualized_return = ((end_value / initial_value) - 1) / ((date_2 - date_1) / 365)
     expect(portfolio.profit(date_1,
-                            date_2)).to eq((s1_price_2 - s1_price_1) + (s2_price_2 - s2_price_1) + (s3_price_2 - s3_price_1))
+                            date_2)).to eq({ net_profit:, annualized_return: })
   end
 
   it "calculates profit correctly for empty portfolio" do
     date_1 = Date.new(2021, 1, 1)
     date_2 = Date.new(2021, 1, 2)
     portfolio = Portfolio.new([])
-    expect(portfolio.profit(date_1, date_2)).to eq 0
+    expect(portfolio.profit(date_1, date_2)).to eq({ net_profit: 0, annualized_return: 0 })
+  end
+
+  it "calculates profit correctly for portfolio with one stock" do
+    stock_1 = Stock.new(1)
+    date_1 = Date.new(2021, 1, 1)
+    date_2 = Date.new(2021, 1, 2)
+    s1_price_1 = stock_1.price(date_1)
+    s1_price_2 = stock_1.price(date_2)
+    expect(s1_price_1).not_to eq(s1_price_2)
+    portfolio = Portfolio.new([stock_1])
+    net_profit = s1_price_2 - s1_price_1
+    annualized_return = ((s1_price_2 / s1_price_1) - 1) / (1 / BigDecimal(365)) # 1/365 of a year, since the dates are one day apart
+    expect(portfolio.profit(date_1,
+                            date_2)).to eq({ net_profit:, annualized_return: })
+  end
+
+  it "calculates profit correctly for portfolio in same day" do
+    stock_1 = Stock.new(1)
+    date_1 = Date.new(2021, 1, 1)
+    portfolio = Portfolio.new([stock_1])
+    expect(portfolio.profit(date_1, date_1)).to eq({ net_profit: 0, annualized_return: 0 })
   end
 end
