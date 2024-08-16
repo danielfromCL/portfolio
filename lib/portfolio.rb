75888ef4f24ef77c010fa100020ae8b8a4089189
danielfromCL <81190002+danielfromCL@users.noreply.github.com>
2024-08-16 00:37:52 -0400

    feat(Portfolio): implement class initializer and signature for profit method

--- lib/portfolio.rb
+++ lib/portfolio.rb
@@ -1,3 +1,17 @@
 # frozen_string_literal: true
+# Class that represents a portfolio of stocks
 class Portfolio
+  attr_accessor :stocks
+
+  def initialize(stocks = [])
+    @stocks = stocks
+  end
+
+  # Calculates the portfolio's profit for a given date range
+  # @param from_date [Date] the start date
+  # @param to_date [Date] the end date
+  # @return [BigDecimal] the profit
+  def profit(from_date, to_date)
+
+  end
 end
