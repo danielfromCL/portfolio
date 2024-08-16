a9af122fb87d9e4e4451d8bac1a022236f1c327d
danielfromCL <81190002+danielfromCL@users.noreply.github.com>
2024-08-16 01:14:49 -0400

    feat(Portfolio): implement profit method, alongisde dummy Stock class

--- lib/portfolio.rb
+++ lib/portfolio.rb
@@ -12,6 +12,10 @@ class Portfolio
   # @param to_date [Date] the end date
   # @return [BigDecimal] the profit
   def profit(from_date, to_date)
-
+    profit = BigDecimal(0)
+    @stocks.each do |stock|
+      profit += stock.price(to_date) - stock.price(from_date)
+    end
+    profit
   end
 end
---
+++ lib/stock.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+# Class that represents a stock
+class Stock
+  attr_reader :id
+
+  # Initializes a new stock with a given id
+  # @param id [Integer] the stock's id
+  def initialize(id)
+    @id = id
+  end
+
+  # Returns the price of the stock at a given date
+  # @param date [Date] the date to get the price for
+  # @return [BigDecimal] the price of the stock at the given date
+  def price(date)
+    BigDecimal(date.to_time.to_i % (100 + @id)) # An id based number generator
+  end
+end
